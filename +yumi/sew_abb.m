408f75a307857f089ae4ad57228557834652fb57
Alex Elias <aelias36@yahoo.com>
2025-03-03 23:21:13 -0500

    Calculate psi_ABB using subproblems

--- +yumi/sew_abb.m
+++ +yumi/sew_abb.m
@@ -10,28 +10,67 @@ classdef sew_abb
             obj.e_r = e_r;
         end
 
-        function [psi, e_r, p_0W, e_x, h_4_0] = fwd_kin(obj, kin, q)
+        function [psi, p_0W, e_x, h_4_0] = fwd_kin(obj, kin, q)
             h_4_0 = rot(kin.H(:,1), q(1)) * rot(kin.H(:,2), q(2)) * rot(kin.H(:,3), q(3)) * kin.H(:,4);
             [~, ~, p_0W] = fwdkin_inter(kin, q, 7);
 
-            % e_r = kin.H(:,1); % Unit vector along axis 1
-            e_r = obj.e_r;
-            w = p_0W; % Position vector of WCP: Intersection of axis 7 and common normal of axes 6 and 7
-            % p_0S = kin.P(:,1);
-            % p_0S = kin.P(:,1)+ rot(kin.H(:,1), q(1))*kin.P(:,2) + rot(kin.H(:,1), q(1)) * rot(kin.H(:,2), q(2)) * kin.P(:,3);
             p_0S = kin.P(:,1);
-            e_3 = h_4_0; % Unit vector along axis 4
+            p_SW = p_0W - p_0S;
             
-            e_x = cross(e_r, w-p_0S) / norm(cross(e_r, w-p_0S));
+            e_x = cross(obj.e_r, p_SW) / norm(cross(obj.e_r, p_SW));
             
             psi = atan2( ...
-                sign(dot(e_3, e_r))*norm(cross(e_x, e_3)),...
-                dot(e_x, e_3));
+                sign(dot(h_4_0, obj.e_r))*norm(cross(e_x, h_4_0)),...
+                dot(e_x, h_4_0));
+        end
+
+        function psi = fwd_kin_sp2(obj, kin, q)
+            % R_03 * h_4 = R(e_x, alpha)*R(e_y, psi)*e_x
+            % Where -pi/2 < alpha < pi/2
+
+            h_4_0 = rot(kin.H(:,1), q(1)) * rot(kin.H(:,2), q(2)) * rot(kin.H(:,3), q(3)) * kin.H(:,4);
+            [~, ~, p_0W] = fwdkin_inter(kin, q, 7);
+
+            p_0S = kin.P(:,1);
+            p_SW = p_0W - p_0S;
+            
+            e_x = cross(obj.e_r, p_SW) / norm(cross(obj.e_r, p_SW));
+            e_y = cross(e_x, obj.e_r);
+
+            [t_alpha, t_psi] = subproblem.sp_2(h_4_0, e_x, -e_x, e_y);
+            if abs(t_alpha(1))<pi/2
+                psi = t_psi(1);
+            else
+                psi = t_psi(end);
+            end
+
+        end
+
+        function psi = fwd_kin_sp4(obj, kin, q)
+            % e_x'* R_03 * h_4 = e_x'*R(e_y, psi)*e_x
+            % where sign(psi) = sign(e_r' * R_03 * h_4)
+            
+            h_4_0 = rot(kin.H(:,1), q(1)) * rot(kin.H(:,2), q(2)) * rot(kin.H(:,3), q(3)) * kin.H(:,4);
+            [~, ~, p_0W] = fwdkin_inter(kin, q, 7);
+
+            p_0S = kin.P(:,1);
+            p_SW = p_0W - p_0S;
+            
+            e_x = cross(obj.e_r, p_SW) / norm(cross(obj.e_r, p_SW));
+            e_y = cross(e_x, obj.e_r);
+
+            t_psi = subproblem.sp_4(e_x, e_x, e_y, e_x'* h_4_0);
+            if sign(dot(h_4_0, obj.e_r)) * t_psi(1) > 0
+                psi = t_psi(1);
+            else
+                psi = t_psi(end);
+            end
+
 
         end
 
         function [J_psi, sign_term] = jacobian(obj, kin, q)
-            [~, e_r, p_0W, e_x, h_4_0] = fwd_kin(obj, kin, q); % TODO move e_r
+            [~, p_0W, e_x, h_4_0] = fwd_kin(obj, kin, q);
 
             % Jacobian of p_0W wrt q
             kin_W = kin;
@@ -47,10 +86,10 @@ classdef sew_abb
             J_h4 = [J_E_full(4:6, :) zeros(3, 4)];
 
             % Jacobian of e_x wrt p_0W
-            J_x_W = obj.jacobian_x_W(e_r, p_0W, e_x);
+            J_x_W = obj.jacobian_x_W(obj.e_r, p_0W, e_x);
 
 
-            alpha = -sign(dot(h_4_0, e_r)) / norm(cross(e_x, h_4_0));
+            alpha = -sign(dot(h_4_0, obj.e_r)) / norm(cross(e_x, h_4_0));
 
             % Jacobian of arm angle wrt R_03*h_4
             J_psi_h4 = alpha * e_x';
@@ -62,7 +101,7 @@ classdef sew_abb
             J_psi = J_psi_W * J_W + J_psi_h4 * J_h4;
             
             % d/dt sign(t) is undefined at t=0
-            sign_term = dot(h_4_0, e_r);
+            sign_term = dot(h_4_0, obj.e_r);
         end
 
         % Jacobian of e_x wrt p_0W
